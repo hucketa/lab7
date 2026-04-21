b360722ee2f0f2c445d3dd6d6a482f7973983487
Hucketa <gunenkoym@gmail.com>
2026-04-21 16:05:27 +0300

    #TASK4: Add example test

---
+++ test/example.test.js
@@ -0,0 +1,9 @@
+describe("Example tests", () => {
+  test("should pass", () => {
+    expect(1 + 1).toBe(2);
+  });
+
+  test("should handle strings", () => {
+    expect("hello".toUpperCase()).toBe("HELLO");
+  });
+});
